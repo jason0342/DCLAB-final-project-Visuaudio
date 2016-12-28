c6d942b6e70d8c9bfe00e23c14bcd3090523b5da
NTUwanderer <harveyycyang@gmail.com>
2016-12-28 15:59:16 +0800

    add always_ff in biquad

--- src/Biquad.sv
+++ src/Biquad.sv
@@ -78,6 +78,14 @@ module Biquad(
 
 	always_ff @(posedge i_clk or posedge i_rst) begin
 		if(i_rst) begin
+			state_r <= S_WAIT;
+			count_r <= 0;
+			A_r <= 1;
+			g_r <= 0;
 		end else begin
+			state_r <= state_w;
+			count_r <= count_w;
+			A_r <= A_w;
+			g_r <= g_w;
 		end
 	end

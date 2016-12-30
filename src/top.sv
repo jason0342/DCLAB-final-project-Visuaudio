a6ba13da96fc77859889578e098d9403776a9b98
jason0342 <jasonwang924@hotmail.com>
2016-12-30 11:21:13 +0800

    implement parts of gain control interface

--- src/top.sv
+++ src/top.sv
@@ -1,6 +1,6 @@
 module top(
-	input i_start, //KEY[3], after debounce
-	input i_stop,
+	input i_select, //KEY[3], after debounce
+	input i_back,
 	input i_up,
 	input i_down, //KEY[0]
 	// input SW02, // 0:normal, 1:change speed ?
@@ -13,15 +13,15 @@ module top(
 	input i_switch, // enable signal
 
 	inout I2C_SDAT,
-	inout [15:0] SRAM_DQ,
+	// inout [15:0] SRAM_DQ,
 
 	output I2C_SCLK,
 	output [19:0] SRAM_ADDR,
-	output SRAM_CE_N,
-	output SRAM_OE_N,
-	output SRAM_WE_N,
-	output SRAM_UB_N,
-	output SRAM_LB_N,
+	// output SRAM_CE_N,
+	// output SRAM_OE_N,
+	// output SRAM_WE_N,
+	// output SRAM_UB_N,
+	// output SRAM_LB_N,
 	output DACDAT,
 	output [4:0] o_timer,
 	output [2:0] o_state,
@@ -30,18 +30,25 @@ module top(
 	output [2:0] o_play_state,
 );
 
-	enum { S_INIT, S_IDLE, S_SETTING } state_r, state_w;
-	enum { FREQ1, FREQ2, FREQ3, FREQ4, FREQ5, FREQ6 } eq_freq_r, eq_freq_w;
+	parameter nBand = 6;
+
+	enum { S_INIT, S_IDLE, S_BAND_SEL, S_SET_GAIN } state_r, state_w;
 
 	logic startI_r, startI_w;
 	logic doneI, doneP, doneR, doneDSP;
 	logic r_data, p_data;
 	logic[31:0] data_r, data_w;
+	logic[2:0] band_r, band_w;
+	logic[2:0] set_band;
+	logic[nBand:0][15:0] gain_r, gain_w;
+	logic[15:0] set_gain;
 
 	assign o_state = state_r;
-	assign SRAM_CE_N = 0;
-	assign SRAM_UB_N = 0;
-	assign SRAM_LB_N = 0;
+	assign set_band = band_r;
+	assign set_gain = gain_r[band_r];
+	// assign SRAM_CE_N = 0;
+	// assign SRAM_UB_N = 0;
+	// assign SRAM_LB_N = 0;
 
 	I2CManager i2cM(
 		.i_start(startI_r),
@@ -104,12 +111,38 @@ always_comb begin
 		S_IDLE: begin
 			startI_w = 0;
 			pos_w = 0;
-			
+			if(i_select) begin
+				state_w = S_BAND_SEL;
+				band_w = 1;
+			end
 		end
 
-		S_SETTING: begin
-			case(eq_freq_r)
-			endcase
+		S_BAND_SEL: begin
+			if(i_back) begin
+				state_w = S_IDLE;
+				band_w = 0;
+			end else if(i_select) begin
+				state_w = S_SET_GAIN;
+			end else if(i_up && band_r != nBand) begin
+				band_w = band_r + 1;
+			end else if(i_down && band_r != 0) begin
+				band_w = band_R - 1;
+			end
+
+		end
+
+		S_SET_GAIN: begin
+			if(i_back) begin
+				state_w = S_BAND_SEL;
+			end else if (i_up) begin
+				if(int'(gain_r[band_r]) <= 12) begin
+					gain_w[band_r] = int'(gain_r[band_r]) + 1;
+				end
+			end else if (i_down) begin
+				if(int'(gain_r[band_r]) >= -12) begin
+					gain_w[band_r] = int'(gain_r[band_r]) - 1;
+				end
+			end
 		end
 
 	endcase
@@ -122,11 +155,19 @@ always_ff @(posedge i_clk or posedge i_rst) begin
 		startI_r <= 1;
 		startP_r <= 0;
 		startR_r <= 0;
+		band_r <= 0;
+		foreach(gain_r[i]) begin
+			gain_r[i] <= 0;
+		end
 	end else begin
 		state_r <= state_w;
 		startI_r <= startI_w;
 		startP_r <= startP_w;
 		startR_r <= startR_w;
+		band_r <= band_w;
+		foreach(gain_r[i]) begin
+			gain_r[i] <= gain_w[i];
+		end
 	end
 end
 
